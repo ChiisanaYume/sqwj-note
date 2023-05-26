bd6646d2e2bfa29a7ae9555a1540c573051f55e3
ChiisanaYume <faintdream@163.com>
2023-05-26 10:08:51 +0800

    Commit git

--- git-push.bat
+++ git-push.bat
@@ -1,3 +1,8 @@
-git add .
-git commit -m "Commit git"
-git push -u origin master
+set log=%USERPROFILE%\Desktop\gitlog%date%.log
+echo ---------- >> %log%
+git add . >> %log% 2>&1
+echo [%date% %time%] >> %log%
+git commit -m "Commit git" >> %log% 2>&1
+echo [%date% %time%] >> %log%
+git push -u origin master >> %log% 2>&1
+echo [%date% %time%] >> %log%
